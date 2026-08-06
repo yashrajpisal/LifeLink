6b0fb03c1f2a144b9f62e7b26046f5b6d22b6681
yashrajpisal <yashraajpisal@gmail.com>
2026-08-06 21:02:00 +0530

    Creating LifeLink Project Structure

---
+++ lifelink1/src/main/java/com/kurukshetra/Main.java
@@ -0,0 +1,12 @@
+package com.kurukshetra;
+
+import com.kurukshetra.view.Welcome;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Shree Ganeshay Namhaa!!");
+        Application.launch(Welcome.class,args);
+    }
+}

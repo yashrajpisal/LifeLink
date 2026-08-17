ba95b7583c34a3556ef6dbc9d4ca1e55933d84b0
jayraj-pisal <jayrajpisal2005@gmail.com>
2026-08-17 19:57:54 +0530

    added main.java

---
+++ lifelink1/src/main/java/com/kurukshetra/Main.java
@@ -0,0 +1,21 @@
+package com.kurukshetra;
+import java.security.AlgorithmConstraints;
+
+import com.kurukshetra.model.HospitalUserModel;
+import com.kurukshetra.view.Welcome;
+import com.kurukshetra.view.admin.AdminDashboard;
+import com.kurukshetra.view.demo.AdminEmergencyDispatch;
+import com.kurukshetra.view.demo.DriverEmergencyMonitor;
+import com.kurukshetra.view.driver.DriverDashboard;
+import com.kurukshetra.view.hospital.HospitalDashboard;
+import com.kurukshetra.view.loginSignup.AalLoginStartPoint;
+import com.kurukshetra.view.police.PoliceDashboard;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Shree Ganeshay Namhaa!!");
+        Application.launch(Welcome.class, args);
+    }
+}
